8eca88434737bf0d63c9a94047db4dbb3a033505
Alan Uecker <fleddermaus2@gmail.com>
2019-02-16 18:24:55 +0100

    added text

--- src/display.ino
+++ src/display.ino
@@ -1,17 +1,20 @@
-#include <ESP8266WiFi.h>
 #include <WiFiClient.h>
+#include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
 #include <ESP8266HTTPClient.h>
+
 #include <Time.h>
 #include <TimeLib.h>
+#include <SPI.h>
+#include <ArduinoJson.h>
+
+#include <PxMatrix.h>
+#include <Ticker.h>
+#include <Fonts/Picopixel.h>
+
+#include "./icons/icons.h"
+#include "secrets.h"
 #include "./OpenWeatherMap.h"
-
-const char *ow_key = "26c131ba210fe0e2794c5a68f821005d";
-const char *nodename = "esp8266-weather";
-const char *wifi_ssid = "notvisable";
-const char *wifi_passwd = "44482477";
-
-//==============================================================================================================
 
 typedef enum wifi_s
 {
@@ -40,10 +43,76 @@ String dateTime(String timestamp)
   return String(buff);
 }
 
+// create ticker
+Ticker display_ticker;
+
+// pins for led matrix
+#define P_LAT 16
+#define P_A 5
+#define P_B 4
+#define P_C 15
+#define P_D 12
+#define P_E 0
+#define P_OE 2
+
+// create display
+PxMATRIX display(64, 32, P_LAT, P_OE, P_A, P_B, P_C, P_D);
+
+// Some standard colors
+uint16_t RED = display.color565(255, 0, 0);
+uint16_t GREEN = display.color565(0, 255, 0);
+uint16_t BLUE = display.color565(0, 0, 255);
+uint16_t WHITE = display.color565(255, 255, 255);
+uint16_t YELLOW = display.color565(255, 255, 0);
+uint16_t CYAN = display.color565(0, 255, 255);
+uint16_t MAGENTA = display.color565(255, 0, 255);
+uint16_t BLACK = display.color565(0, 0, 0);
+
+union single_double {
+  uint8_t two[2];
+  uint16_t one;
+} this_single_double;
+
+// draw weather icons
+void draw_weather_icon(uint8_t icon)
+{
+  if (icon > 10)
+    icon = 10;
+  for (int yy = 0; yy < 10; yy++)
+  {
+    for (int xx = 0; xx < 10; xx++)
+    {
+      uint16_t byte_pos = (xx + icon * 10) * 2 + yy * 220;
+      this_single_double.two[1] = weather_icons[byte_pos];
+      this_single_double.two[0] = weather_icons[byte_pos + 1];
+      display.drawPixel(1 + xx, yy, this_single_double.one);
+    }
+  }
+}
+
+// draw simple text
+void text(uint8_t xpos, uint8_t ypos, String text, uint16_t color)
+{
+  display.setTextColor(color);
+  display.setCursor(xpos, ypos);
+  display.print(text);
+}
+
+// function for ticker
+void display_updater()
+{
+  display.display(70);
+}
+
 void setup()
 {
   Serial.begin(115200);
-  Serial.println("\n\n\n\n");
+
+  display.begin(16);
+  display.clearDisplay();
+  display_ticker.attach(0.002, display_updater);
+
+  display.setFont(&Picopixel);
 
   connectWiFiInit();
   WF_status = W_TRY;
@@ -54,6 +123,7 @@ void currentConditions(void)
   OWM_conditions *ow_cond = new OWM_conditions;
   owCC.updateConditions(ow_cond, ow_key, "de", "koeln", "metric");
   Serial.println("icon: " + ow_cond->icon + ", temp.: " + ow_cond->temp + ", " + ow_cond->description);
+  text(1, 5, "Temp.: " + ow_cond->temp + "C", display.color565(96, 96, 250));
   delete ow_cond;
 }
 
